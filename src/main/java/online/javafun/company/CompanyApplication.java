9ec88ec76797adc78572ff17773395aba6d9e93e
User <user@its.me>
2023-01-04 07:19:41 +0100

    JPQL implementation

--- src/main/java/online/javafun/company/CompanyApplication.java
+++ src/main/java/online/javafun/company/CompanyApplication.java
@@ -2,12 +2,28 @@ package online.javafun.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CompanyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CompanyApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(CompanyApplication.class, args);
+		EmployeeRepository employeeRepository = context.getBean(EmployeeRepository.class);
+		employeeRepository.save(new Employee(1L, "Jan", "Kowalski", 2000.0));
+		employeeRepository.save(new Employee(2L, "Marta", "Dudek", 13000.0));
+		employeeRepository.save(new Employee(3L, "Paweł", "Adamczyk", 12500.0));
+		employeeRepository.save(new Employee(4L, "Kasia", "Kowalska", 4000.0));
+
+		System.out.println(">>> Pracownicy po podwyżce:");
+		employeeRepository.giveRiseToAll();
+		employeeRepository.findAll().forEach(System.out::println);
+
+		System.out.println(">>> Bogaci pracownicy:");
+		employeeRepository.findReachEmploees().forEach(System.out::println);
+
+		System.out.println(">>> Pracownicy zarabiający miedzy 3000 zł a 5000 zł:");
+		employeeRepository.findBySalaryInRange(3000, 5000).forEach(System.out::println);
 	}
 
 }
--- src/main/java/online/javafun/company/EmployeeRepository.java
+++ src/main/java/online/javafun/company/EmployeeRepository.java
@@ -1,5 +1,21 @@
 package online.javafun.company;
 
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-interface EmployeeRepository extends CrudRepository<Employee, Long> { }
+import java.util.List;
+
+interface EmployeeRepository extends CrudRepository<Employee, Long> {
+    @Query("SELECT e FROM Employee e WHERE e.salary > 10000")
+    List<Employee> findReachEmploees();
+
+    @Query("UPDATE Employee e SET e.salary = e.salary + 100")
+    @Modifying
+    @Transactional
+    void giveRiseToAll();
+
+    @Query("SELECT e FROM Employee e WHERE e.salary > :min AND e.salary < :max")
+    List<Employee> findBySalaryInRange(double min, double max);
+}
